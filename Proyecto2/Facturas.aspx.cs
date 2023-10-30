a5e8c791492cdec57c0e31f1f580695d7e363624
AdrianSote <adrianmvparedes@gmail.com>
2023-10-30 21:29:48 +0100

    Initial commit

---
+++ Proyecto2/Facturas.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+
+public partial class Facturas : System.Web.UI.Page
+{
+    private static string cs;
+    private static SqlConnection con;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {     
+        Informacion.DataSource = recolectarInformacion();
+        Informacion.DataBind();
+    }
+
+    protected DataTable recolectarInformacion()
+    {
+        iniciarConexion();
+        SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Facturas", con);
+        DataTable dt = new DataTable();
+        da.Fill(dt);
+        return dt;
+    }
+
+    protected void iniciarConexion()
+    {
+        cs = ConfigurationManager.ConnectionStrings["CONEXION"].ConnectionString;
+        con = new SqlConnection(cs);
+    }
+
+
+}
